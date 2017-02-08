81e4d772b32b35de096f89c8befc80f1c4df7d59
Hank Gontarz <gontarzhd03@mounties.mansfield.edu>
2017-02-08 09:18:35 -0500

    New chessboard

---
+++ src/week3/ChessUI.java
@@ -0,0 +1,12 @@
+package week3;
+
+/**
+ *
+ * @author Hank Gontarz
+ */
+public class ChessUI {
+    public static void main(String[] args) {
+        ChessBoard chess = new ChessBoard();
+        
+    }
+}

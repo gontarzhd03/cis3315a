e697f655364dcd8efd40959cd37b782b6aa2debe
Hank Gontarz <gontarzhd03@mounties.mansfield.edu>
2017-02-08 16:24:47 -0500

    Chess UI

--- src/week3/ChessUI.java
+++ src/week3/ChessUI.java
@@ -1,12 +1,36 @@
 package week3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Hank Gontarz
  */
 public class ChessUI {
     public static void main(String[] args) {
+        int[] rowcol = {0, 0, 0, 0};
         ChessBoard chess = new ChessBoard();
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to chess game!");
+        System.out.println("Pieces use a row and column number");
+        System.out.println("Enter a starting position followed by an ending position");
+        System.out.println(chess.toString());
+        do {
+            System.out.print("From, To: ");
+            String rowcolStr = sc.nextLine();
+            if(rowcolStr.equals("quit")) break;
+            String[] numbers = rowcolStr.split(" ");
+            if(numbers.length >= 4) {
+                for(int j = 0; j < rowcol.length; j++) {
+                    rowcol[j] = Integer.parseInt(numbers[j]);
+                }
+                chess.move(rowcol[0], rowcol[1], rowcol[2], rowcol[3]);
+                System.out.println("");
+                System.out.println(chess.toString());
+            }
+            else {
+               System.out.println("You missed a number!");
+            }
+        } while(true);
     }
 }
